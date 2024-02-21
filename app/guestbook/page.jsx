a3956eb190d64807cb3e20b7016c51514238f082
AbhiVarde <91776428+AbhiVarde@users.noreply.github.com>
2024-02-21 21:23:04 +0530

    feat: added pages & design components

--- app/guestbook/page.jsx
+++ app/guestbook/page.jsx
@@ -4,8 +4,6 @@ const GuestBookPage = () => {
     <div className="max-w-7xl mx-auto md:px-16 px-6 lg:mt-36 md:mt-32 mt-28 ">
       <h1 className="text-3xl font-bold">GuestBook</h1>
       <p className="mt-4">Leave a message :)</p>
-   
-      </div>
     </div>
   );
 };
--- app/page.jsx
+++ app/page.jsx
@@ -31,22 +31,24 @@ export default function Home() {
       <section className="flex xl:flex-row flex-col xl:items-center items-start xl:justify-center justify-between gap-x-12 mb-16">
         {profile &&
           profile.map((data) => (
-            <div key={data._id} className="lg:max-w-2xl max-w-2xl">
-              <Slide>
-                <h1 className="font-semibold tracking-tight text-2xl sm:text-4xl mb-6 lg:leading-[3.7rem] leading-tight lg:min-w-[700px] min-w-full ">
-                  {data.headline}
-                </h1>
-                <p className="text-base leading-relaxed">{data.shortBio}</p>
+            <>
+              <div key={data._id} className="lg:max-w-2xl max-w-2xl">
+                <Slide>
+                  <h1 className="font-semibold tracking-tight text-2xl sm:text-4xl mb-6 lg:leading-[3.7rem] leading-tight lg:min-w-[700px] min-w-full ">
+                    {data.headline}
+                  </h1>
+                  <p className="text-base leading-relaxed">{data.shortBio}</p>
+                </Slide>
+                <Slide delay={0.1}>
+                  <Social type="social" />
+                </Slide>
+                <Slide delay={0.1}></Slide>
+              </div>
+              <Slide delay={0.14}>
+                <HeroSvg />
               </Slide>
-              <Slide delay={0.1}>
-                <Social type="social" />
-              </Slide>
-              <Slide delay={0.1}></Slide>
-            </div>
+            </>
           ))}
-        <Slide delay={0.14}>
-          <HeroSvg />
-        </Slide>
       </section>
       <Job />
     </main>
