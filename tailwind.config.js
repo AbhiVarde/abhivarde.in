b31e19e6b644e1f2be3e4be6bd0c7319b560d76f
Abhi Varde <91776428+AbhiVarde@users.noreply.github.com>
2024-06-08 16:35:25 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,18 +8,12 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        grid: "grid 15s linear infinite",
         scroll:
           "scroll var(--animation-duration, 4s) var(--animation-direction, forwards) linear infinite",
         scrollReverse:
           "scroll var(--animation-duration, 4s) reverse var(--animation-direction, forwards) linear infinite",
       },
-
       keyframes: {
-        grid: {
-          "0%": { transform: "translateY(-50%)" },
-          "100%": { transform: "translateY(0)" },
-        },
         scroll: {
           to: {
             transform: "translate(calc(-25% - 0.25rem))",
