48d0e463dfb746568a50c0f0e782a4f1f5e00172
Abhi Varde <91776428+AbhiVarde@users.noreply.github.com>
2024-06-08 16:36:45 +0530

    Delete app directory

--- app/data/headerNavLinks.jsx
+++
@@ -1,9 +0,0 @@
-const headerNavLinks = [
-  { title: "Home", url: "/" },
-  { title: "Blog", url: "/blog" },
-  { title: "Projects", url: "/projects" },
-  { title: "GuestBook", url: "/guestbook" },
-  { title: "About", url: "/about" },
-];
-
-export default headerNavLinks;
