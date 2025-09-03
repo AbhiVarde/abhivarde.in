7da02683c3dcb4d9bb9c5fdedf60a18bc5b23b26
Abhi Varde <abhivarde99@gmail.com>
2025-09-03 12:27:39 +0530

    refactor(blog): clean up blog cards, remove publish date & description

--- app/components/blog/Blogs.jsx
+++ app/components/blog/Blogs.jsx
@@ -10,41 +10,29 @@ const Blogs = () => {
   const [hoveredIndex, setHoveredIndex] = useState(null);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 mt-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
       {blogs.map((blog, index) => (
-        <div
+        <Link
           key={index}
-          className="group relative transition-all duration-200 rounded-3xl bg-[#111111] border border-[#333] p-4"
+          target="_blank"
+          href={blog.url}
+          onMouseEnter={() => setHoveredIndex(index)}
+          onMouseLeave={() => setHoveredIndex(null)}
+          className="group relative rounded-3xl overflow-hidden border border-[#333] bg-[#111111]"
         >
-          <Link
-            target="_blank"
-            href={blog?.url}
-            onMouseEnter={() => setHoveredIndex(index)}
-            onMouseLeave={() => setHoveredIndex(null)}
-          >
-            <div className="relative w-full h-[200px] sm:h-[250px] rounded-xl overflow-hidden">
-              <Image
-                src={blog.image}
-                alt={blog.title}
-                fill
-                className="object-cover hover:opacity-90 transition duration-300"
-              />
-            </div>
-            <p className="font-medium text-white sm:text-lg mt-2">
-              {blog.title}
-            </p>
-            <p className="flex gap-0.5 text-sm mt-2 text-white/80">
-              {blog.publishdate}{" "}
-              <GoArrowUpRight
-                size={21}
-                className={`${
-                  hoveredIndex === index &&
-                  "text-white transition-all duration-200 -translate-y-0.5 translate-x-1"
-                }`}
-              />
-            </p>
-          </Link>
-        </div>
+          <div className="relative w-full h-[240px]">
+            <Image
+              src={blog.image}
+              alt={blog.title}
+              fill
+              className="object-cover transition-opacity duration-300 group-hover:opacity-90"
+            />
+          </div>
+          <p className="font-medium text-white text-lg p-4">{blog.title}</p>
+          <div className="absolute top-4 right-4 text-white opacity-0 group-hover:opacity-100 transition">
+            <GoArrowUpRight size={21} />
+          </div>
+        </Link>
       ))}
     </div>
   );
